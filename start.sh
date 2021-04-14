89b297c943ccfb3d8e49b61ead0fea265a4869e7
postuond <ondrej.postulka1@gmail.com>
2021-04-14 12:45:58 -0700

    snad uz dneska stacilo

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ then
 cd /root/Jenkins-K8s-Python/charts
 helm install devopsjenkins ./sample-app
 sleep 1m
+sed -i '/sample-app.info/d' /etc/hosts
 ADD=$(kubectl describe pods | grep IP: | awk '{print $2;}' | tail -n 1)
 echo "$ADD sample-app.info" >> /etc/hosts
 curl -v sample-app.info/
