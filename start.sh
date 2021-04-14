f161d50f4f3cb0efb6da8ed5eb671e5c0b115213
postuond <ondrej.postulka1@gmail.com>
2021-04-14 12:37:13 -0700

    snad naposled

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+helm delete devopsjenkins
+sleep 1m
 kubectl get pods | awk '{print $1}' | grep -e "sample-app"
 
 if [[ "$?" == "1" ]]
 then
 cd /root/Jenkins-K8s-Python/charts
 helm install devopsjenkins ./sample-app
-
-elif [[ "$?" == "0" ]]
-then
-helm delete devopsjenkins
-cd /root/Jenkins-K8s-Python/charts
-helm install devopsjenkins ./sample-app
+sleep 1m
+ADD=$(kubectl describe pods | grep IP: | awk '{print $2;}' | tail -n 1)
+echo "$ADD sample-app.info" >> /etc/hosts
+curl -v sample-app.info/
 
 fi
